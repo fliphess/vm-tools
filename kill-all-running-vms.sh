43d64024507a5c758f36612038f49b47eda535c7
Flip <flip@byte.nl>
2014-05-21 23:36:13 +0200

    warn before removing

--- kill-all-running-vms.sh
+++ kill-all-running-vms.sh
@@ -3,7 +3,9 @@ function die() { echo "Error in $0: $1"; exit 1; }
 
 RUNNING=$( virsh list --all | sed 1,2d | grep -vE 'base-|^$'  | awk '{print $2}' ) || die "Failed to get running vm's"
 
+echo "Cleaning up old running VMs"
 for VM in $RUNNING 
 do
-  virsh destroy "$VM" && virsh undefine "$VM" || die "Failed to kill $VM"
+    echo "Removing $VM"
+    virsh destroy "$VM" && virsh undefine "$VM" || die "Failed to kill $VM"
 done
